b53aeabca7e1919d0d9db0cbbda04f41f6e8841b
Reese Ganaway <reeseganaway@gmail.com>
2022-01-13 14:55:30 -0500

    Added login page that gets login info and redirected to an updated userProfile page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import React, { useState } from "react";
 import UserProfile from "./Pages/UserProfile";
+import Login from "./Pages/Login";
 
 function App() {
   const [accountBalance, setAccountBalance] = useState(14568.27);
+
   const [currentUser, setCurrentUser] = useState({
     userName: "Kakashi Hatake",
     memberSince: "09/15/1973",
   });
+
+  const mockLogin = (loginInfo) => {
+    const newUser = { ...currentUser };
+    newUser.userName = loginInfo.userName;
+    setCurrentUser(newUser);
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route
-            path="/Home"
+            path="/home"
             element={<Home accountBalance={accountBalance} />}
           />
           <Route
@@ -26,6 +35,10 @@ function App() {
                 memberSince={currentUser.memberSince}
               />
             }
+          />
+          <Route
+            path="/login"
+            element={<Login user={currentUser} mockLogin={mockLogin} />}
           />
         </Routes>
       </BrowserRouter>
