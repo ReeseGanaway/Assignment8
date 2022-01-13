31331d7d1d3c7230c476ab1e589800d94a671ced
Reese Ganaway <reeseganaway@gmail.com>
2022-01-13 11:28:25 -0500

    first commit, home page already done

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+export default function Home(Props) {
+  return (
+    <div>
+      <img
+        src="https://res.cloudinary.com/andreahabib/image/upload/v1642026304/React_Bank_dk7n1a.png"
+        alt="bank"
+      />
+      <h1>Bank of React</h1>
+    </div>
+  );
+}
